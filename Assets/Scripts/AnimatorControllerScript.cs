9c4fb19a68a43c53533e45f7aca7171f6caf978a
tejas123 <jasani.tejas@gmail.com>
2014-04-25 11:54:11 +0530

    Final
    Final

---
+++ Assets/Scripts/AnimatorControllerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorControllerScript : MonoBehaviour 
+{
+	private Animator animator;
+	private float speed;
+	
+	void Start () 
+	{
+		animator = gameObject.GetComponent<Animator>();
+		StartCoroutine(setSpeed());
+	}
+	
+	//Following enumerator just change the value of Animation Parameter randomly
+	IEnumerator setSpeed()
+	{
+		yield return new WaitForSeconds(1.0f);
+		speed = Random.Range(0,3.0f);
+		//This line set the value of Animation Parameter 
+		animator.SetFloat("StickmanSpeed",speed);
+		StartCoroutine(setSpeed());
+	}	
+}
